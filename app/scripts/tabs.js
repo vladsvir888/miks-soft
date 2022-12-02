f3fd2473c4ebd2513913e3a8d6363bb38291929c
vsvirydovich <v.sviridovich@imedia.by>
2022-12-02 13:30:53 +0300

    refactor tabs

--- app/scripts/tabs.js
+++ app/scripts/tabs.js
@@ -3,34 +3,39 @@ const Tabs = (selector) => {
     return;
   }
 
-  const tabBtns = document.querySelectorAll(`${selector} .js-tabs-btn`);
-  const tabContents = document.querySelectorAll(`${selector} .js-tabs-content`);
+  const section = document.querySelector(selector);
 
-  tabBtns.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-      const parent = btn.closest('.unique');
+  if (!section) return;
 
-      const tabContent = document.querySelector(`${selector} [data-tab-content=${btn.id}]`);
+  section.addEventListener('click', (e) => {
+    const { target } = e;
 
-      if (index === 0) { // todo: решить проблему, если кнопок табов будет больше, чем в дизайне
-        parent.style.setProperty('--tabs-gradient', 'radial-gradient(183.92% 177.93% at 112.29% -107.48%, #483142 0%, rgba(204, 86, 35, 0) 100%)');
-      } else if (index === 1) {
-        parent.style.setProperty('--tabs-gradient', 'radial-gradient(183.92% 177.93% at 112.29% -107.48%, #1E4C73 0%, rgba(204, 86, 35, 0) 100%)');
-      } else {
-        parent.style.setProperty('--tabs-gradient', 'radial-gradient(183.92% 177.93% at 112.29% -107.48%, #271E37 0%, rgba(204, 86, 35, 0) 100%)');
-      }
+    if (!target.classList.contains('js-tabs-btn')) return;
 
-      tabContents.forEach((content) => {
-        content.classList.remove('unique__content--show');
-      });
+    const { id } = target;
 
-      tabBtns.forEach((btn) => {
-        btn.closest('.tabs-nav__item').classList.remove('tabs-nav__item--active');
-      });
+    if (!id) return;
 
-      btn.closest('.tabs-nav__item').classList.add('tabs-nav__item--active');
-      tabContent.classList.add('unique__content--show');
+    const { color } = target.dataset;
+
+    if (color) {
+      section.style.setProperty('--tabs-gradient', `linear-gradient(223.91deg, ${color} -1.11%, rgba(204, 86, 35, 0) 42.01%)`);
+    }
+
+    const tabContent = document.querySelector(`${selector} [data-tab-content="${id}"]`);
+
+    const tabContents = document.querySelectorAll(`${selector} .js-tabs-content`);
+    tabContents.forEach(content => {
+      content.classList.remove('unique__content--show');
     });
+
+    const tabBtns = document.querySelectorAll(`${selector} .js-tabs-btn`);
+    tabBtns.forEach(btn => {
+      btn.closest('.tabs-nav__item').classList.remove('tabs-nav__item--active');
+    });
+
+    target.closest('.tabs-nav__item').classList.add('tabs-nav__item--active');
+    tabContent.classList.add('unique__content--show');
   });
 };
 
