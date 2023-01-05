b8ecae8ad87fb6d3791a00d5756b923578d2fe71
vsvirydovich <v.sviridovich@imedia.by>
2023-01-05 15:07:50 +0300

    fix

--- app/scripts/topBtn.js
+++ app/scripts/topBtn.js
@@ -1,14 +1,25 @@
 const TopBtn = () => {
   const topBtn = document.querySelector('.top-btn');
   const poftfolioSection = document.querySelector('.hero-inner');
+  const secondPortfolioItem = document.querySelector('.portfolio__item:nth-child(1)');
 
-  if (!topBtn) return;
+  if (window.matchMedia('(max-width: 576px)').matches) {
+    poftfolioSection.dataset.offsetTop = secondPortfolioItem.offsetTop;
 
-  topBtn.addEventListener('click', () => {
-    poftfolioSection.scrollIntoView({
-      behavior: 'smooth',
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > poftfolioSection.dataset.offsetTop) {
+        topBtn.classList.add('top-btn--active');
+      } else {
+        topBtn.classList.remove('top-btn--active');
+      }
     });
-  });
+
+    topBtn.addEventListener('click', () => {
+      poftfolioSection.scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  }
 };
 
 export default TopBtn;
